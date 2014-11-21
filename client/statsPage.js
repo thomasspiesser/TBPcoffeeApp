32283cdc8a4b1f6270ab8e5d48837706e8b5828e
Max Schelker <max.schelker@hu-berlin.de>
2014-11-21 13:34:15 +0100

    changes font size and button size in coffeeTable for better readability on tablet. Distinguishes between espresso and cappuccino in bar chart.

--- client/statsPage.js
+++ client/statsPage.js
@@ -1,24 +1,37 @@
 Template.diagram.rendered = function () {
-	var coffeeData = {
-		key: "Total",
+	var coffeeData = [];
+	coffeeData[0] = {
+		key: "Cappuccino",
 		color: "#4f99b4",
 		values: [],
 	};
+	coffeeData[1] = {
+		key: "Espresso",
+		color: "#d67777",
+		values: [],
+	};
+	
 	var users = Meteor.users.find().fetch();
 	users = _.filter(users, function(user){ return user.emails[0].address != 'admin@admin.com'; });
 
-	var sortedUsers = _.sortBy(users, function (user) {return -user.profile.espresso.length - user.profile.cappuccino.length} )
+	var sortedUsers = _.sortBy(users, function (user) {return -user.profile.espresso.length -user.profile.cappuccino.length} )
+	
 	for (var i = 0; i < sortedUsers.length; i++) {
 		var user = sortedUsers[i];
-		var coffee = user.profile.espresso.length + user.profile.cappuccino.length;
-		var name = user.profile.name;
-
 		var userData = {
-			name: name,
-			coffee: coffee
+			name: user.profile.name,
+			coffee: user.profile.cappuccino.length
 		};
+		coffeeData[0].values.push(userData);
+	};
 
-		coffeeData.values.push(userData);
+	for (var i = 0; i < sortedUsers.length; i++) {
+		var user = sortedUsers[i];
+		var userData = {
+			name: user.profile.name,
+			coffee: user.profile.espresso.length
+		};
+		coffeeData[1].values.push(userData);
 	};
 
 	nv.addGraph(function() {
@@ -29,14 +42,14 @@ Template.diagram.rendered = function () {
 		.showValues(true)           //Show bar value next to each bar.
 		.tooltips(true)             //Show tooltips on hover.
 		.transitionDuration(350)
-		// .stacked(true)
-		.showControls(false);        //Allow user to switch between "Grouped" and "Stacked" mode.
+		.stacked(true)
+		.showControls(true);        //Allow user to switch between "Grouped" and "Stacked" mode.
 
 		chart.yAxis
-		.tickFormat(d3.format(',.2f'));
+		.tickFormat(d3.format(',f'));
 
 		d3.select('#coffeeChart svg')
-		.datum([coffeeData])
+		.datum(coffeeData)
 		.call(chart);
 
 		nv.utils.windowResize(chart.update);
