e3fd73ba30837cfed016165c92aef583dda6d661
Max Schelker <max.schelker@hu-berlin.de>
2014-12-08 09:22:22 +0100

    adds second stats page (not yet fully functional)

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'TBP Coffee App',
+  description: 'The coffee counting app of the TBP group',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'public/logo.png' ,
+  'android_mdpi': 'public/logo.png' ,
+  'android_hdpi': 'public/logo.png' ,
+  'android_xhdpi': 'public/logo.png' 
+});
